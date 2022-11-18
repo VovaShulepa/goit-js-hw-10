823ae556fad37091df3aeb1b5883e2dc0425987e
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-11-19 01:57:20 +0200

    finish

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,7 +4,7 @@ export const fetchCountries = name => {
   // let finalURL = `${API_URL}${name}?fields=name,capital,population,flags,languages`;
 
   return fetch(
-    `${API_URL}${name}?fields=name,capital,population,flags,languages`
+    `${API_URL}${name}?fields=name,capital,population,flags,languages,continents`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
